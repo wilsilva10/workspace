195bdb0aa1453a70f38455f943277421fe5aee19
wilber viana <wilberoliveira1@hotmail.com>
2021-06-17 19:18:48 -0300

    Desafio 02 - 1.1

--- Desafio/src/Desafio02.java
+++ Desafio/src/Desafio02.java
@@ -23,36 +23,60 @@ public class Desafio02 {
 //		  Vitórias: ??
 //		  Derrotas: ??
 //		  Empate: ??
-		
+
 		Object[] opcoes = { "PEDRA", "PAPEL", "TESOURA" };
 		int usuario = JOptionPane.showOptionDialog(null, "GAME DESAFIO - ESCOLHA A OPÇÃO: ", "**JoKenPô**", 0,
 				JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
 		int computador = gerarPc(3);
-//		Object[] retorno = { "SIM", "NÃO" };
+		
+		
 //		int ret = JOptionPane.showOptionDialog(null, "GAME DESAFIO - ESCOLHA A OPÇÃO: ", "**JoKenPô**", 0, JOptionPane.YES_NO_OPTION, null,
 //				retorno, retorno[0]);
 //		
-		String escolhaPc = "";
+		// Chamada para saber qual a escolha do usuario
+		String escolhaU = "";
+		if (usuario == 0) {
+			escolhaU = "PEDRA";
+		} else if (usuario == 1) {
+			escolhaU = "PAPEL";
+		} else {
+			escolhaU = "TESOURA";
+		}
+		// Chamada para saber qual a escolha do usuario
+		String escolhaPc = " ";
 		switch (computador) {
 		case 0:
 			escolhaPc = "PEDRA";
+			break;
 		case 1:
 			escolhaPc = "PAPEL";
+			break;
 		case 2:
 			escolhaPc = "TESOURA";
+			break;
 		}
-		
-		String msg = "";
-
+		// Logica do jogo para saber quem é o vencedor
+		String result = "";
 		if (usuario == computador) {
-			msg = "Eu("+usuario+")"+" X Pc("+escolhaPc+")"+"\nEmpate";
+			result = "Empate ";
 		} else if ((usuario == 0 && computador == 2) || (usuario == 2 && computador == 1)
 				|| (usuario == 1 && computador == 0)) {
-			msg = "Eu("+usuario+")"+" X Pc("+escolhaPc+")"+"\nUsuario ganhou !";
+			result = "Vitória";
 		} else {
-			msg = "Eu("+usuario+")"+" X Pc("+escolhaPc+")"+"\nComputador ganhou !";
+			result = "Derrota";
+			
 		}
-		JOptionPane.showMessageDialog(null, msg);
+		
+		
+		Object[] jogo = { "SIM", "NÃO"};
+//		JOptionPane.showMessageDialog(null,"Eu (" + escolhaU + ") " + " X Pc (" + escolhaPc + ")" + "\nResultado: "+ result);
+		int resultado = JOptionPane.showOptionDialog(null, "Eu (" + escolhaU + ") " + " X Pc (" + escolhaPc + ")" + 
+		"\nResultado: "+ result + "\nDeseja jogar novamente? ", 
+		"**JoKenPô**", 0, JOptionPane.INFORMATION_MESSAGE, null,
+		opcoes, opcoes[0]);
+	  if(resultado == 0) {
+		  System.out.println(resultado);
+	  }
 	}
 
 }
