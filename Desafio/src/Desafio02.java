f929acd043d0f31f356eeee1bbeed18b22fd1a0c
wilber viana <wilberoliveira1@hotmail.com>
2021-06-21 15:59:21 -0300

    Finalização desafio 1.4

--- Desafio/src/Desafio02.java
+++ Desafio/src/Desafio02.java
@@ -24,11 +24,7 @@ public class Desafio02 {
 //		  Derrotas: ??
 //		  Empate: ??
 
-		Object[] opcoes = { "PEDRA", "PAPEL", "TESOURA" };
-		int usuario = JOptionPane.showOptionDialog(null, "GAME DESAFIO - ESCOLHA A OPÇÃO: ", "**JoKenPô**", 0,
-				JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
-		int computador = gerarPc(3);
-		Object[] jogo = { "SIM", "NÃO"};
+		
 		int e = 0;
 		int d = 0;
 		int v = 0;
@@ -38,50 +34,55 @@ public class Desafio02 {
 //		
 		// Chamada para saber qual a escolha do usuario
 		do {
-		String escolhaU = "";
-		if (usuario == 0) {
-			escolhaU = "PEDRA";
-		} else if (usuario == 1) {
-			escolhaU = "PAPEL";
-		} else {
-			escolhaU = "TESOURA";
-		}
-		// Chamada para saber qual a escolha do computador
-		String escolhaPc = " ";
-		switch (computador) {
-		case 0:
-			escolhaPc = "PEDRA";
-			break;
-		case 1:
-			escolhaPc = "PAPEL";
-			break;
-		case 2:
-			escolhaPc = "TESOURA";
-			break;
-		}
-		// Logica do jogo para saber o resultado
-	
-		String result = "";
-		if (usuario == computador) {
-			result = "Empate ";
-			e++;
-		} else if ((usuario == 0 && computador == 2) || (usuario == 2 && computador == 1)
-				|| (usuario == 1 && computador == 0)) {
-			result = "Vitória";
-			v++;
-		} else {
-			result = "Derrota";
-			d++;
-			
-		}
+			Object[] opcoes = { "PEDRA", "PAPEL", "TESOURA" };
+			int usuario = JOptionPane.showOptionDialog(null, "GAME DESAFIO - ESCOLHA A OPÇÃO: ", "**JoKenPô**", 0,
+					JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+			int computador = gerarPc(3);
+			Object[] jogo = { "SIM", "NÃO"};
+			String escolhaU = "";
+			if (usuario == 0) {
+				escolhaU = "PEDRA";
+			} else if (usuario == 1) {
+				escolhaU = "PAPEL";
+			} else {
+				escolhaU = "TESOURA";
+			}
+			// Chamada para saber qual a escolha do computador
+			String escolhaPc = " ";
+			switch (computador) {
+			case 0:
+				escolhaPc = "PEDRA";
+				break;
+			case 1:
+				escolhaPc = "PAPEL";
+				break;
+			case 2:
+				escolhaPc = "TESOURA";
+				break;
+			}
+			// Logica do jogo para saber o resultado
 		
-		 resultado = JOptionPane.showOptionDialog(null, "Eu (" + escolhaU + ") " + " X Pc (" + escolhaPc + ")" + 
-		"\nResultado: "+ result + "\nDeseja jogar novamente? ", 
-		"**JoKenPô**", 0, JOptionPane.INFORMATION_MESSAGE, null,
-		jogo, jogo[0]);
+			String result = "";
+			if (usuario == computador) {
+				result = "Empate ";
+				e++;
+			} else if ((usuario == 0 && computador == 2) || (usuario == 2 && computador == 1)
+					|| (usuario == 1 && computador == 0)) {
+				result = "Vitória";
+				v++;
+			} else {
+				result = "Derrota";
+				d++;
+				
+			}
+			
+			 resultado = JOptionPane.showOptionDialog(null, "Eu (" + escolhaU + ") " + " X Pc (" + escolhaPc + ")" + 
+			"\nResultado: "+ result + "\nDeseja jogar novamente? ", 
+			"**JoKenPô**", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			jogo, jogo[0]);
 		} while(resultado !=1); 
 			JOptionPane.showMessageDialog(null, "Placar final \n Vitórias: "+v+"\n Derrotas: "+d+"\n Empates: "+e );
-		
+			
 		
 		
 	}
